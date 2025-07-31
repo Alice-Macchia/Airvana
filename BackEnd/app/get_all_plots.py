c9942f324b48baa54fb724f748bde16a347a5562
Lorenzo Iuliano <lorenzo.iulianokk@gmail.com>
2025-07-31 14:04:55 +0200

    test #3

--- BackEnd/app/get_all_plots.py
+++ BackEnd/app/get_all_plots.py
@@ -4,13 +4,11 @@ from BackEnd.app.models import Plot
 from geoalchemy2.shape import to_shape
 
 async def get_all_plots_coords(db: AsyncSession):
-    result = await db.execute(select(Plot.id, Plot.centroid))
-
+    result = await db.execute(select(Plot.id, Plot.centroid))  # ✅ FIX
     plots = []
     for plot_id, geometry in result.all():
         if geometry:
             shapely_geom = to_shape(geometry)
-            centroid = shapely_geom.centroid  # redundant, puoi già usare shapely_geom
             plots.append({
                 "plot_id": plot_id,
                 "lat": shapely_geom.y,
